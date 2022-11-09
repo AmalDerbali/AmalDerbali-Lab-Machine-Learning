f2bc36ddeac212c7ebfec3f05e5c578548252cc9
AmalDerbali <89392991+AmalDerbali@users.noreply.github.com>
2022-11-10 00:14:12 +0100

    add prediction.py file

--- production_model.py
+++ production_model.py
@@ -1,16 +1,22 @@
 import joblib
-import pandas 
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
-#ref: https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
+#ref: 
+# https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
+# https://stackoverflow.com/questions/48947194/add-randomforestclassifier-predict-proba-results-to-original-dataframe
 
 prediction_model = joblib.load('Prediction_model')
-data= pandas.read_csv('test_samples.csv')
+data= pd.read_csv('test_samples.csv')
 
 probability=prediction_model.predict_proba(data)
 prediction= prediction_model.predict(data)
 
-prediction_df= pandas.DataFrame(probability, prediction, 
-                                columns=['probability class 0', 'probability class 1', 'prediction'])
+prob= pd.DataFrame(probability, 
+                       columns= ['probability class 0', 'probability class 1'])
+pred= pd.DataFrame(prediction,
+                        columns=['prediction'])
 
-prediction_df.to_csv('prediction.csv')
+
+df=pd.concat([prob, pred], axis=1)
+df.to_csv('prediction.csv')
+
